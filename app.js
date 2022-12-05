bfc0b31be975dcc2052e5699f0b21c583c5f1657
upto <jordanhouyon@outlook.fr>
2022-12-05 14:33:21 +0100

    pas fini

--- app.js
+++ app.js
@@ -31,10 +31,17 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen,peugeot, ford] // Vos voitures dans cette propriété !
 }
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
+
+for (let index of voitures.collection) {
+
+
+
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
-
-
+    peugeot.ouvrirPorte();
+    citroen.ouvrirPorte();
+    ford.ouvrirPorte();
+}
